3928fce6a65e8f7f5880f927c41fb45aface2232
sreejithaccu <60417708+sreejithaccu@users.noreply.github.com>
2023-06-18 11:05:52 -0700

    responsive issue fix

--- src/components/Prompt/Prompt.js
+++ src/components/Prompt/Prompt.js
@@ -4,19 +4,24 @@ import { ImEnter } from 'react-icons/im';
 import '../Prompt/Prompt.css';
 
 const Prompt = () => {
+    const handleIconClick = () => {
+        // Handle the onClick event for the ImEnter icon here
+        console.log('ImEnter icon clicked!');
+        // Add your custom logic or function calls here
+    };
     return (
-            <Form>
-                <Form.Group controlId="exampleForm.ControlTextarea">
-                    <div className="textarea-container">
-                        <Form.Control
-                            as="textarea"
-                            className="chat-textarea"
-                            placeholder="Describe the image you would like to generate"
-                        />
-                        <ImEnter className="icon" />
-                    </div>
-                </Form.Group>
-            </Form>
+        <Form>
+            <Form.Group controlId="exampleForm.ControlTextarea">
+                <div className="textarea-container">
+                    <Form.Control
+                        as="textarea"
+                        className="chat-textarea"
+                        placeholder="Describe the image you would like to generate"
+                    />
+                    <ImEnter className="icon" onClick={handleIconClick} />
+                </div>
+            </Form.Group>
+        </Form>
     );
 };
 
